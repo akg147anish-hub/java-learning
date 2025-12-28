3aa90ee90ec322d30a61b9b6140b1401e87cbb7a
Anish Gupta <akg147.anish@gmail.com>
2025-12-28 23:56:46 +0530

    Basics: Area of Hexagon using user input

---
+++ basics/AreaofHexagon.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class AreaofHexagon{
+    public static void main (String[] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the side: ");
+        double side = sc.nextDouble();
+        double area = (3* Math.sqrt(3)/2) * side * side;
+        System.out.println("The area of the hexagon is " + area);
+    }
+}
