85d2c9f8e8da48bdd632067f5b7ecfe215b8fb9e
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-04-02 23:45:37 +0530

    Diamond Pattern

---
+++ Pattern/Pattern7.java
@@ -0,0 +1,31 @@
+public class Pattern7 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        // Upper pyramid
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for(int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        // Lower pyramid
+        for(int i = n - 1; i >= 1; i--) {
+            for(int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for(int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
