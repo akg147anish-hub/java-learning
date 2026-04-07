b49c3a30986366391a12f681708db07531e9b328
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-04-07 23:53:25 +0530

    Butterfly Pattern

---
+++ Pattern/Pattern12.java
@@ -0,0 +1,47 @@
+public class Pattern12 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        // Upper Part
+        for(int i = 1; i <= n; i++) {
+
+            // Left stars
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // Spaces
+            for(int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // Right stars
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // Lower Part
+        for(int i = n; i >= 1; i--) {
+
+            // Left stars
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // Spaces
+            for(int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // Right stars
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
