c0cd75649841fb6afd4a7ece66b030d4b62dc33b
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-04-13 23:56:46 +0530

    Add rectangle class with area calculation method

---
+++ OOP/RectangleArea.java
@@ -0,0 +1,18 @@
+class Rectangle {
+    int length;
+    int breadth;
+
+    int area() {
+        return length * breadth;
+    }
+}
+
+public class RectangleArea {
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle();
+        r.length = 10;
+        r.breadth = 5;
+
+        System.out.println("Area: " + r.area());
+    }
+}
