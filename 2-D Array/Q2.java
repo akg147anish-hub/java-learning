29238ba8fd3a3cfbe6504edc100cdc115c3f504a
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-05-13 00:10:28 +0530

    Print elements using 2-D Array
    Fix nested loop increment for proper array traversal.

--- 2-D Array/Q2.java
+++ 2-D Array/Q2.java
@@ -4,6 +4,7 @@ class Q2{
             {1,2,3},
             {4,5,6}
         };
+        
         for(int i=0; i<arr.length; i++){
             for(int j= 0; j< arr[0].length; j++){
                 System.out.print(arr[i][j] + " ");
