c6d20b0e3a042da8b800a7d83354421835f0046d
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-05-16 23:39:36 +0530

    Add largest element finder in 2D Array

---
+++ 2-D Array/Q4.java
@@ -0,0 +1,24 @@
+class Q4 {
+    public static void main(String[] args) {
+
+        int[][] arr = {
+            {1, 9, 3},
+            {4, 5, 6}
+        };
+
+        int max = arr[0][0];
+
+        for(int i = 0; i < arr.length; i++) {
+
+            for(int j = 0; j < arr[i].length; j++) {
+
+                if(arr[i][j] > max) {
+
+                    max = arr[i][j];
+                }
+            }
+        }
+
+        System.out.println("Largest = " + max);
+    }
+}
