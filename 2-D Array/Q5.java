d0ee92d15b12fb6acc2a8380ff9a4333268f13d4
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-05-17 23:48:27 +0530

    Add smallest element finder in 2D array

---
+++ 2-D Array/Q5.java
@@ -0,0 +1,24 @@
+class Q5 {
+    public static void main(String[] args) {
+
+        int[][] arr = {
+            {8, 2, 7},
+            {4, 5, 6}
+        };
+
+        int min = arr[0][0];
+
+        for(int i = 0; i < arr.length; i++) {
+
+            for(int j = 0; j < arr[i].length; j++) {
+
+                if(arr[i][j] < min) {
+
+                    min = arr[i][j];
+                }
+            }
+        }
+
+        System.out.println("Smallest = " + min);
+    }
+}
