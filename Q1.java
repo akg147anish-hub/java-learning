495575982c409312be4bc863e0c1e37267d98ea6
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-05-29 23:41:23 +0530

    Define Node class and implement linked list

---
+++ Q1.java
@@ -0,0 +1,32 @@
+class Node {
+
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Q1 {
+
+    public static void main(String[] args) {
+
+        Node first = new Node(10);
+        Node second = new Node(20);
+        Node third = new Node(30);
+
+        first.next = second;
+        second.next = third;
+
+        Node temp = first;
+
+        while(temp != null) {
+
+            System.out.println(temp.data);
+
+            temp = temp.next;
+        }
+    }
+}
