81e1d1c5598cad8bb0dd003510cc6ff963eb6465
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-05-30 23:30:41 +0530

    Implement basic linked list with node count

---
+++ Linked list/Q2.java
@@ -0,0 +1,32 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class Q2 {
+
+    public static void main(String[] args) {
+
+        Node first = new Node(10);
+        Node second = new Node(20);
+        Node third = new Node(30);
+
+        first.next = second;
+        second.next = third;
+
+        int count = 0;
+
+        Node temp = first;
+
+        while(temp != null) {
+            count++;
+            temp = temp.next;
+        }
+
+        System.out.println("Total Nodes = " + count);
+    }
+}
