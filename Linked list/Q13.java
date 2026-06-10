abc3df59acd8c4598018f256bc48f330ceee742f
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-06-10 23:30:17 +0530

    Implement linked list and count even nodes

---
+++ Linked list/Q13.java
@@ -0,0 +1,37 @@
+class Node {
+
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Q13 {
+
+    public static void main(String[] args) {
+
+        Node head = new Node(10);
+        head.next = new Node(21);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(45);
+        head.next.next.next.next = new Node(50);
+
+        int count = 0;
+
+        Node temp = head;
+
+        while(temp != null) {
+
+            if(temp.data % 2 == 0) {
+                count++;
+            }
+
+            temp = temp.next;
+        }
+
+        System.out.println("Even Nodes = " + count);
+    }
+}
