08553bece25f5a3bb43a6de1bf0b7a9bb9b24f5f
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-06-14 23:28:10 +0530

    Search element in linked list

---
+++ Linked list/Q17.java
@@ -0,0 +1,41 @@
+class Node {
+
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class Q17 {
+
+    public static void main(String[] args) {
+
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(50);
+
+        int key = 50;
+
+        boolean found = false;
+
+        Node temp = head;
+
+        while(temp != null) {
+
+            if(temp.data == key) {
+                found = true;
+                break;
+            }
+
+            temp = temp.next;
+        }
+
+        if(found)
+            System.out.println("Found");
+        else
+            System.out.println("Not Found");
+    }
+}
