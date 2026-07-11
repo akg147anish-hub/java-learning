3d4fb1df1fa998b086eb90249fbd590c0da51b8a
ANISH KUMAR GUPTA <akg147.anish@gmail.com>
2026-07-11 23:39:18 +0530

    Add PrintQueue class to demonstrate queue usage

---
+++ Queue/PrintQueue.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class PrintQueue {
+    public static void main(String[] args) {
+
+        Queue<Integer> q = new LinkedList<>();
+
+        q.offer(1);
+        q.offer(2);
+        q.offer(3);
+
+        System.out.println(q);
+    }
+}
